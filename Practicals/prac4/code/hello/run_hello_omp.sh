bf5c88df268619d83a664b085477e8cae1dd777f
teaching70 <teaching70@arcus-htc-login01.arcus-htc.arc.local>
2020-06-15 16:39:35 +0100

    Changed script in OMP pracs to use 8 threads so work on GPU nodes of arcus-htc

--- Practicals/prac4/code/hello/run_hello_omp.sh
+++ Practicals/prac4/code/hello/run_hello_omp.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=8
 #SBATCH --time=00:05:00
 #SBATCH --job-name=hello_omp
 #SBATCH --gres=gpu:1
 
 module purge
 module load gcc/4.9.2
+
+export OMP_NUM_THREADS=8
 
 ./hello_omp
--- Practicals/prac4/code/integral/run_integral_omp.sh
+++ Practicals/prac4/code/integral/run_integral_omp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=8
 #SBATCH --time=00:05:00
 #SBATCH --job-name=integral
 #SBATCH --gres=gpu:1
@@ -9,6 +9,6 @@
 module purge
 module load gcc/4.9.2
 
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=8
 
 ./integral_omp < integral.inp
